9628c83bdc0a30b153c19e905aaa05fc4b866a6c
Nagendra Naren <nagendra.n@ecoihub.com>
2026-09-22 11:29:33 +0530

    privacy policy related code commits

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -17,7 +17,6 @@ export default function Footer() {
               { name: "Results", href: "/#results" },
               { name: "Contact", href: "/#contact" },
               { name: "Blog", href: "/blog" },
-              { name: "Privacy Policy", href: "/privacy-policy/" },
             ].map((link) => (
               <Link
                 key={link.name}
